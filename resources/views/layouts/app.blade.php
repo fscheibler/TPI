6a257b3d192448fb691db4b07584c983cf00bc95
fscheibler <fscheibler@firstpoint.ch>
2024-02-26 11:36:29 +0100

    modification du layout, ajout d'un footer avec lien vers la doc

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,6 +31,18 @@
             <main>
                 {{ $slot }}
             </main>
+
+            <footer class="bg-white shadow">
+                <div class="flex justify-end mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    <a href="/" class="inline-flex items-center hover:text-blue-600 transition-colors duration-200 ease-in-out">
+                        Documentation
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 ml-1">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+                        </svg>
+                    </a>
+                </div>
+            </footer>
+
         </div>
     </body>
 </html>
